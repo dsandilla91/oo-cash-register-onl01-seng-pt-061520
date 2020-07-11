e4f5a5b3e75120f16354e983f687af448b99af3e
Deborah Santillan <s.debb@yahoo.com>
2020-07-11 17:45:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister 
+  attr_accessor :discount, :item, :last_transaction, :total
 
+  def initialize(discount = 0)
+    @items = []
+    @discount = discount
+    @total = 0 
+  end 
+  
+  def add_item(cost, name, number = 1)
+    self.total = cost * number
+    @items << name
+  end
+  
+  def add_item_2(cost, name, number = 1)
+    self.total = cost * number
+
+  def apply_discount 
+  # the cash register was initialized with an employee discount:  
+  end 
+  
+  def items 
+  # (self).items!?
+  # returns an array containing all items that have been added
+  end
+  
+  def void_last_transaction
+  # subtracts the last transaction from the total 
+  end 
+  
+end 
